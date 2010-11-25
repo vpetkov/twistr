7ce30ab104a95dc8f8e419ddc2a3a4434f3068c3
Enric Morales <me@enric.me>
2010-11-25 01:55:45 +0100

    v0.3, now with more 20% tags support!

--- twistr.sh
+++ twistr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Twistr v0.2, a Tumblr client.
+# Twistr v0.3, a Tumblr client.
 #
 # by Enric Morales (http://enric.me/twistr). Uncopyrighted 2010.
 
@@ -12,16 +12,6 @@ echo "Save your Tumblr email and password in this format at $HOME/.twistr.rc:
    password="YOUR PASSWORD HERE"" && exit 1
 # }}
 
-
-# {{ If filename's missing, print usage. If the title's missing, read from stdin
-if test -z "$1"; then
-	echo -e "Twistr, a tumblr client.\n"
-	echo "Usage: $0 text </path/to/file.txt> \"[title]\""
-	echo "       $0 photo </path/to/photo.jpg> \"[caption]\""
-	exit 1
-fi
-#}}
-
 # {{ cURL magic!
 case $1 in
    text)  if [ -z "$3" ]
@@ -29,11 +19,17 @@ case $1 in
 		else title="$3"
 	  fi
 
+	  if [ -z "$4" ]
+	  	then echo -n "Enter the post tags: " && read tags
+		else tags="$4"
+	  fi
+
    	  curl  -S \
 		-d email=$email \
 		-d password=$password \
 		-d type=regular \
 		-d title="$title" \
+		-d tags="$tags" \
 		-d generator=Twistr \
 		--data-urlencode body@"$2" \
 		"http://www.tumblr.com/api/write" ;;
@@ -43,18 +39,24 @@ case $1 in
 		else caption="$3"
 	  fi
 
+	  if [ -z "$4" ]
+	  	then echo -n "Enter the photo tags: " && read tags
+		else tags="$4"
+	  fi
+
 	  curl  -S \
 		-F email=$email \
 		-F password=$password \
 		-F type=photo \
 		-F generator=Twistr \
 		-F caption="$caption" \
+		-F tags="$tags" \
 		-F "data=@"$2"" \
 		"http://www.tumblr.com/api/write" ;;
 
    *)	echo -e "Twistr, a tumblr client.\n"
-	echo "Usage: $0 text </path/to/file.txt> \"[title]\""
-	echo "       $0 photo </path/to/photo.jpg> \"[caption]\""
+	echo "Usage: $0 text </path/to/file.txt> \"[title]\" \"[tags]\""
+	echo "       $0 photo </path/to/photo.jpg> \"[caption]\" \"[tags]\""
 	exit 1 ;;
 esac
 #}}
