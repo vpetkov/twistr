fa131ffe46d01ee6846a8b68c8c3a34291e27db0
Enric Morales <me@enric.me>
2010-11-25 01:12:00 +0100

    v0.2, with photo support. Usage changed, so run with no arguments.

--- twistr.sh
+++ twistr.sh
@@ -1,43 +1,60 @@
 #!/bin/bash
 #
-# Twistr: a simple bash script to post to Tumblr.
+# Twistr v0.2, a Tumblr client.
 #
-# version 0.1 24 Nov 2010. Uncopyrighted.
-# by Enric Morales (http://enric.me/twistr)
+# by Enric Morales (http://enric.me/twistr). Uncopyrighted 2010.
 
 
 # {{ See if we can import the info on ~/.twistr.rc, otherwise prompt the user.
-
 source ~/.twistr.rc 2>/dev/null && test -z "$email" || test -z "$password" && \
 echo "Save your Tumblr email and password in this format at $HOME/.twistr.rc:
    email="YOUR EMAIL HERE"
    password="YOUR PASSWORD HERE"" && exit 1
-
 # }}
 
 
 # {{ If filename's missing, print usage. If the title's missing, read from stdin
-test -z "$1" && echo "Usage: $0 </path/to/file.txt> \"[title]\"" && exit 1
-test -z "$2" && echo -n "Enter title: " && read title
-
+if test -z "$1"; then
+	echo -e "Twistr, a tumblr client.\n"
+	echo "Usage: $0 text </path/to/file.txt> \"[title]\""
+	echo "       $0 photo </path/to/photo.jpg> \"[caption]\""
+	exit 1
+fi
 #}}
 
-# {{ Tumblr API variables
-posttype='regular'
-title="$2"
-
-# }}
-
-
 # {{ cURL magic!
+case $1 in
+   text)  if [ -z "$3" ]
+   		then echo -n "Enter the post title: " && read title
+		else title="$3"
+	  fi
 
-curl  -S \
-      -d email=$email \
-      -d password=$password \
-      -d type=$posttype \
-      -d title=$title \
-      -d generator=Twistr \
-      --data-urlencode body="$(cat $1)" \
-       "http://www.tumblr.com/api/write"
+   	  curl  -S \
+		-d email=$email \
+		-d password=$password \
+		-d type=regular \
+		-d title="$title" \
+		-d generator=Twistr \
+		--data-urlencode body@"$2" \
+		"http://www.tumblr.com/api/write" ;;
 
+   photo) if [ -z "$3" ]
+   		then echo -n "Enter the photo caption: " && read caption
+		else caption="$3"
+	  fi
+
+	  curl  -S \
+		-F email=$email \
+		-F password=$password \
+		-F type=photo \
+		-F generator=Twistr \
+		-F caption="$caption" \
+		-F "data=@"$2"" \
+		"http://www.tumblr.com/api/write" ;;
+
+   *)	echo -e "Twistr, a tumblr client.\n"
+	echo "Usage: $0 text </path/to/file.txt> \"[title]\""
+	echo "       $0 photo </path/to/photo.jpg> \"[caption]\""
+	exit 1 ;;
+esac
 #}}
